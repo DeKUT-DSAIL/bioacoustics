e909f05f5a0c0ed059d78a0616585db2aed76e1b
kiariegabriel <gabrielkiarie1@gmail.com>
2020-10-07 16:03:05 +0300

    Added a functionality to turn off the raspberry pi at 9 am

--- alarm_final.py
+++ alarm_final.py
@@ -2,36 +2,55 @@
 import os
 import sys
 from adc import *
+from rtc import *
 from time import sleep
 
-gate_pulse = digitalio.DigitalInOut(board.D18) #Set the GPIO PIN 3 as a digital I/O pin
-gate_pulse.direction=digitalio.Direction.OUTPUT #Set GPIO PIN 3 as an output
+
+t=rtc.datetime #read the rtc time
+
+depth_of_discharge=2.8  #voltage
+shutdown_time=9 #9.00 am
+
+gate_pulse = digitalio.DigitalInOut(board.D18) #Set the GPIO PIN 3 as a d$
+gate_pulse.direction=digitalio.Direction.OUTPUT #Set GPIO PIN 3 as an out$
 
 
 try:
-    count=0 # Initialize count at zero 
+    count=0 # Initialize count at zero
     while True:
+        if t.tm_hour>=shutdown_time:
+            alarm()                          #Call the alarm function to schedule wake up for tomorrow at 5.00 am
+            gate_pulse.value=True            #Set GPIO pin 18 high to latch the timer circuit
+            sleep(0.5)
+            gate_pulse.value=False
+            os.system('sudo shutdown now')  #Shutdown the Pi
         voltage=volt()
         print(voltage)
         count+=1
         if count%10==0:
-            voltage_csv()  #Call the function to save the voltage in a csv file after every 300 seconds == 5 minutes
-        if voltage>2.8 and voltage<=3:
-            print('Voltage is low')
-        elif voltage<2.8:
-            print('voltage is extremely low!!!')
+            voltage_csv()  #Call the function to save the voltage in a csv file
+
+        #if voltage>2.8 and voltage<=3:
+        #    print('Voltage is low')
+
+        if voltage<=depth_of_discharge:
+            #print('voltage is extremely low!!!')
             sleep(120)
             voltage=volt()
             voltage_csv()
-            if voltage>2.8:
+            if voltage>depth_of_discharge:
                 print(voltage,'\nBattery has recovered.')
-            elif voltage<=2.8:
+            elif voltage<=depth_of_discharge:
                 print('The system will shutdown in a few.')
-                alarm()                                      #Call the alarm function to set the alarm.
-                gate_pulse.value=True #Set GPIO pin 18 high to latch the timer circuit
+                alarm()                                      #Call the al$
+                gate_pulse.value=True #Set GPIO pin 18 high to latch the $
                 sleep(0.5)
                 gate_pulse.value=False
-                os.system('sudo shutdown now') #Shutdown the Pi 
+                os.system('sudo shutdown now') #Shutdown the Pi
         sleep(30)
 except KeyboardInterrupt:
         sys.exit()
+
+
+
+s
