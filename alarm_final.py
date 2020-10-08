6f2b976bdfbecb3ebab592c27e17a6476fdec3b3
kiariegabriel <gabrielkiarie1@gmail.com>
2020-10-08 16:00:44 +0300

    Added a shutdown function

--- alarm_final.py
+++ alarm_final.py
@@ -6,7 +6,7 @@ from rtc import *
 from time import sleep
 
 
-t=rtc.datetime #read the rtc time
+
 
 depth_of_discharge=2.8  #voltage
 shutdown_time=9 #9.00 am
@@ -14,16 +14,21 @@ shutdown_time=9 #9.00 am
 gate_pulse = digitalio.DigitalInOut(board.D18) #Set the GPIO PIN 3 as a d$
 gate_pulse.direction=digitalio.Direction.OUTPUT #Set GPIO PIN 3 as an out$
 
+def rpi_shutdown():
+    print('The system will shutdown in a few.')
+    alarm()                                      #Call the al$
+    gate_pulse.value=True #Set GPIO pin 18 high to latch the $
+    sleep(0.5)
+    gate_pulse.value=False
+    os.system('sudo shutdown now') #Shutdown the Pi
 
-try:
-    count=0 # Initialize count at zero
-    while True:
+count=0 # Initialize count at zero
+
+while True:
+    try:
+        t=rtc.datetime #read the rtc time
         if t.tm_hour>=shutdown_time:
-            alarm()                          #Call the alarm function to schedule wake up for tomorrow at 5.00 am
-            gate_pulse.value=True            #Set GPIO pin 18 high to latch the timer circuit
-            sleep(0.5)
-            gate_pulse.value=False
-            os.system('sudo shutdown now')  #Shutdown the Pi
+            rpi_shutdown()
         voltage=volt()
         print(voltage)
         count+=1
@@ -41,16 +46,11 @@ try:
             if voltage>depth_of_discharge:
                 print(voltage,'\nBattery has recovered.')
             elif voltage<=depth_of_discharge:
-                print('The system will shutdown in a few.')
-                alarm()                                      #Call the al$
-                gate_pulse.value=True #Set GPIO pin 18 high to latch the $
-                sleep(0.5)
-                gate_pulse.value=False
-                os.system('sudo shutdown now') #Shutdown the Pi
+                rpi_shutdown()
         sleep(30)
-except KeyboardInterrupt:
+    except KeyboardInterrupt:
         sys.exit()
 
 
 
-s
+
