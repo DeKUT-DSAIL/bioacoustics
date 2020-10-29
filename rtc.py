c7428a1e26615703a0252216d817c053a734ff9c
kiariegabriel <gabrielkiarie1@gmail.com>
2020-10-29 16:42:45 +0300

    Increased flexibility

--- rtc.py
+++ rtc.py
@@ -31,8 +31,8 @@ def time_dict():
      object in a dictionary.
     """
 
-    t=rtc.datetime
-    time_dict={'tm_year':t.tm_year,
+    t = rtc.datetime
+    time_dict = {'tm_year':t.tm_year,
            'tm_mon':t.tm_mon,
            'tm_mday':t.tm_mday,
            'tm_hour':t.tm_hour,
@@ -48,12 +48,12 @@ def time_dict():
 def weekdays_shift():
     """ Shifts the weekdays by one and reset to zero
      when day of the week goes beyond six
-     Monday=0, Tuesday=1 ... Saturday=5, Sunday=6"""
+     Monday = 0, Tuesday = 1 ... Saturday = 5, Sunday = 6"""
 
-    t=time_dict()
-    t['tm_wday']+=1
-    if t['tm_wday']>6:
-        t['tm_wday']=0
+    t = time_dict()
+    t['tm_wday'] += 1
+    if t['tm_wday'] > 6:
+        t['tm_wday'] = 0
     return t['tm_wday']
 
 
@@ -62,8 +62,8 @@ def year_shift():
     it is 31st December and the alarm needs to be set
     for the following day which will be a new year"""
 
-    t=time_dict()
-    t['tm_year']+=1
+    t = time_dict()
+    t['tm_year'] += 1
     return t
 
 def months_shift():
@@ -71,33 +71,44 @@ def months_shift():
     it is the end of a month and the alarm needs to be set
     for the following day which will be a new month"""
 
-    t=time_dict()
-    t['tm_mon']+=1
-    if t['tm_mon']>12:
-        t=year_shift()
-        t['tm_mon']=1
+    t = time_dict()
+    t['tm_mon'] += 1
+    if t['tm_mon'] > 12:
+        t = year_shift()
+        t['tm_mon'] = 1
     return t
 
-def next_day():
+def next_day(hour):
     """ Sets the alarm at 5.00 am the following day"""
 
-    t=time_dict()
-    days = {1:31, 2:28, 3:31, 4:30, 5:31, 6:30, 7:31, 8:31, 9:30, 10:31, 11:30, 12:31}
-    t['tm_mday']+=1
-    t['tm_wday']=weekdays_shift()
-    if t['tm_mday']<=days[t['tm_mon']]:
-        t=(t['tm_year'],t['tm_mon'],t['tm_mday'],5,0,0,t['tm_wday'],-1,-1)
-    elif t['tm_mday']>days[t['tm_mon']]:
-        t=months_shift()
-        t['tm_wday']=weekdays_shift()
-        t['tm_mday']=1
-        t=(t['tm_year'],t['tm_mon'],t['tm_mday'],5,0,0,t['tm_wday'],-1,-1)
+    t = time_dict()
+    days = {1:31, 2:28, 3:31, 4:30, 5:31, 6:30,
+            7:31, 8:31, 9:30, 10:31, 11:30, 12:31}
+    t['tm_mday'] += 1
+    t['tm_wday'] = weekdays_shift()
+    if t['tm_mday'] <= days[t['tm_mon']]:
+        t = (t['tm_year'],
+            t['tm_mon'],
+            t['tm_mday'],
+            hour, 0, 0,
+            t['tm_wday'],
+            -1,-1)
+    elif t['tm_mday'] > days[t['tm_mon']]:
+        t = months_shift()
+        t['tm_wday'] = weekdays_shift()
+        t['tm_mday'] = 1
+        t = (t['tm_year'],
+            t['tm_mon'],
+            t['tm_mday'],
+            hour,0,0,
+            t['tm_wday'],
+            -1,-1)
     return t
 
-def alarm():
+def alarm(hour):
     """ Set the alarm at 5.00 am the following day."""
-
-    t=next_day()
+    hour = hour
+    t = next_day(hour)
     rtc.alarm1 = (time.struct_time(t), "daily")
     if rtc.alarm1_status:
         #print("wake up!")
