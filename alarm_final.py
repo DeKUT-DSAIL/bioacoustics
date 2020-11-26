92a1d11960f5c05b657f207e3396a8bed1229cbd
kiariegabriel <gabrielkiarie1@gmail.com>
2020-11-26 17:53:02 +0300

    Added the command line arguments parsing functionality

--- alarm_final.py
+++ alarm_final.py
@@ -1,8 +1,7 @@
 import sys
-import adc
-import rtc
 import board
 import busio
+import logging
 import argparse
 import digitalio
 import subprocess
@@ -11,49 +10,125 @@ from datetime import datetime
 
 sleep(30) #delay for thirty seconds to have the pi's time set
 
-DEPTH_OF_DISCHARGE = 2.8 #voltage
-
-SHUTDOWN_HOUR = 10 #10.00am
-
-gate_pulse = digitalio.DigitalInOut(board.D18) #Set the GPIO PIN 3 as a d$
-gate_pulse.direction = digitalio.Direction.OUTPUT #Set GPIO PIN 3 as an out$
+logging.basicConfig(filename='power_management.log', level=logging.DEBUG)
 
 
 
-def rpi_shutdown():
-    print('The system will shutdown in a few.')
-    rtc.alarm()                                      #Call the al$
-    gate_pulse.value = True #Set GPIO pin 18 high to gate trigger the timer circuit thyristor
-    sleep(0.5)
-    gate_pulse.value = False
-    subprocess.call(['sudo', 'shutdown' ,'now']) #Shutdown the Pi
+try:
 
-count = 0 # Initialize count at zero
+    import adc
+    import rtc
 
-while True:
-    try:
-        t = datetime.now() #get the current time of the pi
-        hour = int(t.strftime('%H'))
-        if hour >= SHUTDOWN_HOUR:
-            rpi_shutdown()
-        voltage = adc.volt()
-        print(voltage)
-        count += 1
-        if count % 10 == 0:
-            adc.voltage_csv()  #Call the function to save the voltage in a csv file
+    parser = argparse.ArgumentParser(description='Monitors the batter voltage and control wake up and shutdown of the system')
 
-        if voltage <= DEPTH_OF_DISCHARGE:
-            sleep(120) #sleep for 2 minutes
-            voltage = adc.volt()
-            adc.voltage_csv()
-            if voltage > DEPTH_OF_DISCHARGE:
-                print(voltage,'\nBattery has recovered.')
-            elif voltage <= DEPTH_OF_DISCHARGE:
+    parser.add_argument('-d_d',
+                            '--depth_of_discharge',
+                            type=int,
+                            default=2.8,
+                            metavar='',
+                            help='The cut off voltage.')
+
+    parser.add_argument('-w_h',
+                            '--wake_hour',
+                            type=int,
+                            default=5,
+                            metavar='',
+                            help='The hour (24 hour clock system) the pi should wake up.')
+
+
+    parser.add_argument('-s_h',
+                            '--shutdown_hour',
+                            type=int,
+                            default=11,
+                            metavar='',
+                            help='The hour in 24 hour clock system the pi should shutdown.')
+
+    parser.add_argument('-s_n',
+                            '--storage_name',
+                            type=str,
+                            default='none',
+                            metavar='',
+                            help="""Name of the external storage. Default is 'none'. If no storage
+                            name is passed the program will store the battery voltage readings
+                            in the SD card. The external device should not be named as 'none'!!!""")
+
+    parser.add_argument('-d_n',
+                            '--directory_name',
+                            type=str,
+                            default='battery',
+                            metavar='',
+                            help='Name of directory to store datestamped csv file containing voltage readings')
+
+    args = parser.parse_args()
+
+    def sd_card():
+        """Returns a string which is the path to store recordings in the SD card.
+        Called when there is no external storage, or when the system
+        can't writ to the external storage"""
+
+        folder_path = os.path.join('/home/pi/', args.directory_name)
+        return folder_path
+
+
+    def external_storage():
+        """Returns a string which is the path to store recordings in the external storage.
+        Called when the user plugs in an external storage device and parse its name in
+        command line"""
+
+        folder_path = os.path.join('/media/pi/', args.storage_name, args.directory_name)
+        return folder_path
+
+
+    if args.storage_name != 'none':
+        try:
+            folder_path = external_storage()
+            if not os.path.exists(folder_path):
+                os.makedirs(folder_path)
+        except Exception as storage_error:
+            logging.info(str(storage_error))
+            folder_path = sd_card()
+            if not os.path.exists(folder_path):
+                os.makedirs(folder_path)
+
+
+    gate_pulse = digitalio.DigitalInOut(board.D18) #Set the GPIO PIN 3 as a d$
+    gate_pulse.direction = digitalio.Direction.OUTPUT #Set GPIO PIN 3 as an out$
+
+
+
+    def rpi_shutdown():
+        print('The system will shutdown in a few.')
+        rtc.alarm(args.wake_hour)    #Call the alarm function to schedule next wake up
+        gate_pulse.value = True #Set GPIO pin 18 high to gate trigger the timer circuit thyristor
+        sleep(0.5)
+        gate_pulse.value = False
+        subprocess.call(['sudo', 'shutdown' ,'now']) #Shutdown the Pi
+
+    count = 0 # Initialize count at zero
+
+    while True:
+        try:
+            t = datetime.now() #get the current time of the pi
+            hour = int(t.strftime('%H'))
+            if hour >= args.shutdown_hour:
                 rpi_shutdown()
-        sleep(30)
-    except KeyboardInterrupt:
-        sys.exit()
+            voltage = adc.volt()
+            print(voltage)
+            count += 1
+            if count % 10 == 0:
+                adc.voltage_csv(voltage, folder_path)  #Call the function to save the voltage in a csv file
 
+            if voltage <= args.depth_of_discharge:
+                sleep(120) #sleep for 2 minutes
+                voltage = adc.volt()
+                adc.voltage_csv()
+                if voltage > args.depth_of_discharge:
+                    print(voltage,'\nBattery has recovered.')
+                elif voltage <= args.depth_of_discharge:
+                    rpi_shutdown()
+            sleep(30)
+        except KeyboardInterrupt:
+            sys.exit()
 
-
-
+except Exception as err:
+    logging.info(str(err))
