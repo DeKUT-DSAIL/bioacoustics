edeb6eeedb02426b35bb72911cd9a84730585d55
kiariegabriel <gabrielkiarie1@gmail.com>
2020-11-27 16:55:14 +0300

    logging functionality added

--- audio_data_collection.py
+++ audio_data_collection.py
@@ -13,11 +13,13 @@ from datetime import datetime
 
 sleep(30) #sleep for thirty seconds to wait the Pi's time to be set.
 
-logging.basicConfig(filename='data.log', level=logging.DEBUG)
+logging.basicConfig(filename='data.log',
+                    level=logging.DEBUG,
+                    format='%(asctime)s:%(levelname)s:%(message)s')
 
 try:
 
-    
+
 
     parser = argparse.ArgumentParser(description='Detect audio activity and save 10s long audio files')
     parser.add_argument('-c',
@@ -85,48 +87,9 @@ try:
 
     BLOCKSIZE = int((args.block_length / 1e3) * args.samplerate) #number of samples to process
 
-    CALIBRATION_BLOCKS = int(args.calibration_duration / (args.block_length / 1e3)) #number of blocks to set the still condition
+    NUM_OF_CALIBRATION_BLOCKS = int(args.calibration_duration / (args.block_length / 1e3)) #number of blocks to set the still condition
 
-    SAMPLES_SAVED = int((args.samplerate / BLOCKSIZE) * args.length_of_saved_audio) #number of samples to save as a single file
-
-
-    t = datetime.now()
-    name_by_date = t.strftime('%Y-%m-%d')
-
-
-    def sd_card():
-        """Returns a string which is the path to store recordings in the SD card.
-        Called when there is no external storage, or when the system
-        can't writ to the external storage"""
-
-        folder_path = os.path.join('/home/pi/', args.directory_name, name_by_date)
-        return folder_path
-
-
-    def external_storage():
-        """Returns a string which is the path to store recordings in the external storage.
-        Called when the user plugs in an external storage device and parse its name in 
-        command line"""
-
-        folder_path = os.path.join('/media/pi/', args.storage_name, args.directory_name, name_by_date)
-        return folder_path
-
-
-    if args.storage_name != 'none':
-        try:
-            folder_path = external_storage()
-            if not os.path.exists(folder_path):
-                os.makedirs(folder_path)
-        except Exception as storage_error:
-            logging.info(str(storage_error))
-            folder_path = sd_card()
-            if not os.path.exists(folder_path):
-                os.makedirs(folder_path)
-
-    else:
-        folder_path = sd_card()
-        if not os.path.exists(folder_path):
-            os.makedirs(folder_path)
+    NUM_OF_BLOCKS_TO_SAVE = int((args.samplerate / BLOCKSIZE) * args.length_of_saved_audio) #number of blocks to save as a single file
 
 
     q = queue.Queue()
@@ -144,7 +107,7 @@ try:
         condition for the purpose of activity detection"""
 
         l = []
-        for _ in range(CALIBRATION_BLOCKS):
+        for _ in range(NUM_OF_CALIBRATION_BLOCKS):
             block = q.get()
             block = block.flatten()
             energy = np.sum(block ** 2)
@@ -163,8 +126,29 @@ try:
         energy = np.sum(my_block ** 2)
         return energy, my_block
 
+    def sd_card(name_by_date):
+        """Returns a string which is the path to store recordings in the SD card.
+        Called when there is no external storage, or when the system
+        can't writ to the external storage"""
 
-    def audio_file_save(data):
+        folder_path = os.path.join('/home/pi/', args.directory_name, name_by_date)
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
+
+        return folder_path
+
+
+    def external_storage(name_by_date):
+        """Returns a string which is the path to store recordings in the external storage.
+        Called when the user plugs in an external storage device and parse its name in 
+        command line"""
+
+        folder_path = os.path.join('/media/pi/', args.storage_name, args.directory_name, name_by_date)
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
+        return folder_path
+
+    def audio_file_save(folder_path, current_time, data, name_by_date):
         """ Saves recorded sound in an external storage.
         It monitors the remaining storage and stops saving
         the files when it is almost full but instead records
@@ -172,17 +156,20 @@ try:
 
         Args: data- a numpy array containing audio samples."""
 
-        t = datetime.now()
-        name_by_time = t.strftime('%H:%M:%S') + '.wav'
+        name_by_time = current_time + '.wav' #timestamp for the audio file name
         usage = disk_usage(folder_path)
         if usage.used / usage.total < args.storage_threshold:
             file_path = os.path.join(folder_path, name_by_time)
+            if args.channels != 1:
+                data = np.reshape(data,
+                                    (int(args.samplerate * args.length_of_saved_audio),
+                                    args.channels))
             sf.write(file_path , data, args.samplerate)
 
         else:
             name = os.path.join(folder_path, name_by_date+'.txt')
             f = open(name, 'a')
-            f.write(t.strftime('%H:%M:%S') + '\t Activity Detected \n')
+            f.write(current_time + '\t Activity Detected \n')
             f.close()
 
     def main():
@@ -194,6 +181,7 @@ try:
 
                 mean, std_dev = calibration()
                 while True:
+                    t = datetime.now()
                     energy, my_block = block_energy()
                     std_deviation = energy - mean
 
@@ -201,12 +189,27 @@ try:
                         print('Activity detected')
                         blocks_of_interest = np.array(my_block)
 
-                        for _ in range(SAMPLES_SAVED - 1):
+                        for _ in range(NUM_OF_BLOCKS_TO_SAVE - 1):
                             my_block = q.get()
                             my_block = my_block.flatten()
                             blocks_of_interest = np.concatenate((blocks_of_interest, my_block))
 
-                        audio_file_save(blocks_of_interest)
+                        name_by_date = t.strftime('%Y-%m-%d')
+                        current_time = t.strftime('%H-%M-%S')
+                        try:
+                            if args.storage_name != 'none':
+                                folder_path = external_storage(name_by_date)
+                                audio_file_save(folder_path, current_time, blocks_of_interest, name_by_date)
+
+                            else:
+                                folder_path = sd_card(name_by_date)
+                                audio_file_save(folder_path, current_time, blocks_of_interest, name_by_date)
+
+                        except Exception as storage_error:
+                            logging.info(str(storage_error))
+                            folder_path = sd_card(name_by_date)
+                            audio_file_save(folder_path, current_time, blocks_of_interest, name_by_date)
+
 
 
         except KeyboardInterrupt:
