9429db9cfdd5274d7c20db7ac3b7de6b9699c119
Gabriel Kiarie <67686554+kiariegabriel@users.noreply.github.com>
2021-10-16 12:07:43 +0300

    installing RPi.GPIO

--- raspi_setup.sh
+++ raspi_setup.sh
@@ -28,6 +28,7 @@ source bioacoustics-env/bin/activate
 pip3 install --upgrade pip
 pip3 install wheel
 pip3 install cython
+pip3 install RPi.GPIO
 pip3 install numpy
 pip3 install pandas
 pip3 install soundfile
